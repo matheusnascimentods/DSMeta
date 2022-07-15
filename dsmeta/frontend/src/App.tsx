2f386654002b28c2175cf271c5eba4c7f8034664
Matheus-dev58 <matheus.nita78@gmail.com>
2022-07-14 22:24:44 -0300

    Os primeiros componentes React foram criados

--- dsmeta/frontend/src/App.tsx
+++ dsmeta/frontend/src/App.tsx
@@ -1,16 +1,20 @@
+import LineTable from "./Components/TableLine"
+
 function App() {
  
     return (  
     
-      <div>
+      <>
 
         <h1>O NENE VOLTOU PRO VASCO</h1>  
       
         <p>O NENE VOLTOU PRO VASCO JOGA MUITA BOLA AINDA</p>
         <p>VEIO MOSTRAR A COLINA ESSA É A TROPA DO VOVÔ</p>
 
-      </div>
-    
+        <LineTable />
+
+      </>
+
     )
 }
 
---
+++ dsmeta/frontend/src/Components/TableLine/index.tsx
@@ -0,0 +1,30 @@
+import NotificationButton from "../NotificationButton"
+
+function LineTable() {
+ 
+    return (  
+    
+        <>
+        
+            <tr>
+
+                <td className="show992">02</td>
+                <td>Anakin</td>
+                <td className="show576">01/10/2006</td>
+                <td className="show992">50</td>
+                <td className="show992">20</td>
+                <td>R$ 40.000,00</td>
+                <td>
+
+                    <NotificationButton />
+
+                </td>
+
+            </tr>
+
+        </>
+    
+    )
+}
+
+export default LineTable
