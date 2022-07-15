a7125c72a3b27b96a1e4c728a57100b40b57c154
Matheus-dev58 <matheus.nita78@gmail.com>
2022-07-14 21:50:00 -0300

    Linhas e arquivos padrões de um projeto react foram apagados

--- dsmeta/frontend/src/App.tsx
+++ dsmeta/frontend/src/App.tsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
+ 
+    return (  
+    
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+        <h1>O NENE VOLTOU PRO VASCO</h1>  
+      
+        <p>O NENE VOLTOU PRO VASCO JOGA MUITA BOLA AINDA</p>
+        <p>VEIO MOSTRAR A COLINA ESSA É A TROPA DO VOVÔ</p>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    
+    )
 }
 
 export default App
