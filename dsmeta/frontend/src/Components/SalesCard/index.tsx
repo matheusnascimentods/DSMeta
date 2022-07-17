9bc3545626a2f124fab8476ca0267abe6cdf9aef
Matheus-dev58 <matheus.nita78@gmail.com>
2022-07-17 19:37:39 -0300

    O Front-End renderiza os dados da API

--- dsmeta/frontend/src/Components/SalesCard/index.tsx
+++ dsmeta/frontend/src/Components/SalesCard/index.tsx
@@ -4,13 +4,26 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 import './styles.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { BASE_URL } from '../../utils/request';
+import { Sale } from '../../models/Sale';
 
 function SalesCard() {
 
     const [minDate, setMinDate] = useState(new Date());
     const [maxDate , setMaxDate]= useState(new Date());
+
+    const [sales, setSales] = useState<Sale[]>([]);
+  
+    useEffect(() => {
+
+        axios.get(`${BASE_URL}/Sales`)
+            .then(response => { setSales(response.data.content); })
  
+    }, []);
+ 
+
     return (  
     
         <>
@@ -67,18 +80,31 @@ function SalesCard() {
 
                     <tbody>
 
-                        <tr>
-                            <td className="show992">03</td>
-                            <td>Anakin</td>
-                            <td className="show576">01/10/2006</td>
-                            <td className="show992">10</td>
-                            <td className="show992">80</td>
-                            <td>R$ 40.000,00</td>
-                            <td>
-                                <NotificationButton />
-                            </td>
+                        {
 
-                        </tr>
+                            sales.map(sale => {
+
+                                return (
+
+                                    <tr key={sale.id}>
+                                    
+                                        <td className="show992">{sale.id}</td>
+                                        <td>{sale.sellerName}</td>
+                                        <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                                        <td className="show992">{sale.visited}</td>
+                                        <td className="show992">{sale.deals}</td>
+                                        <td>{sale.amount.toFixed(2)}</td>
+                                        <td>
+                                            <NotificationButton />
+                                        </td>
+    
+                                    </tr>
+
+                                )
+
+                            }) 
+
+                        }
             
                     </tbody>
     
